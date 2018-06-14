480b396a52ff7b486ef8d484684eaa54a795e32b
greystar <lyndonp80@gmail.com>
2018-06-13 17:58:11 -0700

    HW9 Final Commit

--- App.js
+++ App.js
@@ -1,60 +1,109 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { FlatList, Text, View, Image, Button, StyleSheet } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Home',
+  };
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          !
+        <Text style={{fontSize: 20, color: 'grey'}}>
+          WSDOT & SDOT Traffic Cams
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Button
+          onPress= { () => navigate("Cameras") }
+          title="Traffic Cameras"
+          color="steelblue"
+          />
       </View>
     );
   }
 }
 
 
+class CameraScreen extends Component {
+
+  static navigationOptions = {
+    title: 'Cameras',
+  };
+
+  state = {
+    data: []
+  };
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const response = await fetch('https://web6.seattle.gov/Travelers/api/Map/Data?zoomId=18&type=2');
+    const json = await response.json();
+    this.setState({ data: json.Features });
+  };
+
+  cameraType(camera) {
+      if(camera.Type == 'sdot'){
+            return  "http://www.seattle.gov/trafficcams/images/"+camera.ImageUrl;
+      }else{
+            return "http://images.wsdot.wa.gov/nw/"+camera.ImageUrl;
+      }
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+
+      <View style={styles.container}>
+
+        <FlatList
+          data={this.state.data}
+          // x is the object 
+          keyExtractor={(x, index) => index.toString()}
+          renderItem={ ({item}) =>
+            <View style={styles.textM}>
+             <Image
+                source = {{ uri: this.cameraType(item.Cameras[0]) }}
+                style = {{height: 250, margin: 3}}
+                />
+
+              <Text style={{fontSize: 20, color: 'steelblue'}}>
+                {`${item.Cameras[0].Description}`}
+              </Text>
+            </View>
+
+          }
+         />
+      </View>
+
+    );
+  }
+}
+
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 0,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  textM: {
+    marginBottom: 30
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+});
+
+export default class App extends Component {
+  render () {
+    return <NavigationApp />;
+  }
+}
+
+const NavigationApp = StackNavigator({
+    Home: { screen: HomeScreen },
+    Cameras: { screen: CameraScreen },
 });
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
+import HomeScreen from './App';
+import ProfileScreen from './App'; 
 
 
 AppRegistry.registerComponent('greyreactnative', () => App);
